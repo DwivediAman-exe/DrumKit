e54c0e4a2c3c8c32a4f19b6d09d5ad9e382433c1
Aman Dwivedi <19dwivediaman@gmail.com>
2020-12-26 11:58:56 +0530

    added js file

---
+++ index.js
@@ -0,0 +1,61 @@
+function sound(x) {
+
+  switch (x) {
+    case "w":
+      var tom1 = new Audio('sounds/tom-1.mp3');
+      tom1.play();
+      break;
+    case "a":
+      var tom2 = new Audio('sounds/tom-2.mp3');
+      tom2.play();
+      break;
+    case "s":
+      var tom3 = new Audio('sounds/tom-3.mp3');
+      tom3.play();
+      break;
+    case "d":
+      var tom4 = new Audio('sounds/tom-4.mp3');
+      tom4.play();
+      break;
+    case "j":
+      var snare = new Audio('sounds/snare.mp3');
+      snare.play();
+      break;
+    case "k":
+      var crash = new Audio('sounds/crash.mp3');
+      crash.play();
+      break;
+    case "l":
+      var kick = new Audio('sounds/kick-bass.mp3');
+      kick.play();
+    default:
+      console.log(x);
+  }
+
+}
+
+// detecting button press
+
+document.addEventListener('keydown', function(event) {
+  sound(event.key);
+  buttonAnimation(event.key);
+});
+
+// detecting the click
+
+for (var i = 0; i <= document.querySelectorAll(".drum").length; i++) {
+  document.querySelectorAll("button")[i].addEventListener("click", function() {
+    var buttonInnerHTML = this.innerHTML;
+    sound(buttonInnerHTML);
+    buttonAnimation(buttonInnerHTML);
+  });
+
+}
+
+function buttonAnimation(x){
+  var active = document.querySelector("."+x);
+  active.classList.add("pressed");
+  setTimeout(function(){
+    active.classList.remove("pressed");
+  },100);
+}
